636e2b81ac1f8aa23c3a7256c110f4e2bc3e04f4
erobertsdev <elias@eroberts.dev>
2020-03-27 16:10:34 -0600

    made search separate function

--- src/js/search.js
+++ src/js/search.js
@@ -2,28 +2,50 @@ import { key } from './key.js';
 
 const currentLocationBtn = document.getElementById('location-current');
 
+const search = async (lat, lon) => {
+	try {
+		const response = await axios.get('https://www.hikingproject.com/data/get-trails?', {
+			// https://www.hikingproject.com/data - URL for params
+			params: {
+				key: key,
+				lat: lat,
+				lon: lon,
+				maxDistance: 30,
+				maxResults: 12,
+				// Can also be 'quality'
+				sort: 'quality'
+			}
+		});
+
+		console.log(response.data);
+		renderTrails(response.data, 'current');
+	} catch (error) {
+		console.log(error);
+		return;
+	}
+};
+
 const renderTrails = (data, domLocation) => {
 	const trailList = data.trails;
 
 	for (let trail of trailList) {
 		let dom = document.getElementById(`${domLocation}`);
+		let url = trail.url;
 		let name = trail.name;
-		let type = trail.type;
 		let summary = trail.summary;
 		let difficulty = trail.difficulty;
 		let rating = trail.stars;
 		let location = trail.location;
-		let thumb = trail.imgMedium;
+		let thumb = trail.imgMedium; // !== '' ? trail.imgMedium : 'NO IMAGE AVAILABLE';
 
 		const trailContainer = document.createElement('div');
 		trailContainer.classList.add('trail');
 		trailContainer.innerHTML = `
-        <a href="#">
+        <a href="${url}" target="_blank">
                 <h5 class="trail-name">${name}</h5>
                 <h6 class="trail-location">${location}</h6>
                 <img class="trail-thumb" src="${thumb}" />
                 <p class="trail-summary">${summary}</p>
-                <p class="trail-type">Type: ${type}</p>
                 <p class="trail-difficulty">Difficulty: ${difficulty}</p>
                 <p class="trail-rating">Rating: ${rating}</p>
             </a>
@@ -34,23 +56,7 @@ const renderTrails = (data, domLocation) => {
 };
 
 currentLocationBtn.addEventListener('click', () => {
-	const search = async (lat, lon) => {
-		try {
-			const response = await axios.get('https://www.hikingproject.com/data/get-trails?', {
-				params: {
-					key: key,
-					lat: lat,
-					lon: lon
-				}
-			});
-			currentLocationBtn.style.display = 'none';
-			renderTrails(response.data, 'current');
-		} catch (error) {
-			console.log(error);
-			return;
-		}
-	};
-
+	currentLocationBtn.style.display = 'none';
 	navigator.geolocation.getCurrentPosition(async function success(pos) {
 		const coords = pos.coords;
 		const lat = coords.latitude;
